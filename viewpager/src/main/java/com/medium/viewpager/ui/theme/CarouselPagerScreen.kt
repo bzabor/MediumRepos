f2c3ac17bfef6cff352888a224db7511e4623109
Bob Zabor <bobzabor@detroitlabs.com>
2022-10-26 09:54:24 -0400

    Adding buttons for navigation

--- viewpager/src/main/java/com/medium/viewpager/ui/theme/CarouselPagerScreen.kt
+++ viewpager/src/main/java/com/medium/viewpager/ui/theme/CarouselPagerScreen.kt
@@ -2,11 +2,10 @@ package com.medium.viewpager.ui.theme
 
 import android.util.Log
 import android.widget.Toast
-import androidx.compose.animation.*
+import androidx.compose.animation.Crossfade
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -16,17 +15,15 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.airbnb.lottie.LottieComposition
 import com.airbnb.lottie.compose.*
 import com.google.accompanist.pager.*
 import com.medium.viewpager.R
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
@@ -42,35 +39,63 @@ fun CarouselPagerScreen() {
      * swiped.
      *
     Surface(modifier = Modifier
-        .fillMaxSize()
-        .pointerInput(Unit) {
-        detectDragGestures { change, dragAmount ->
-            change.consume()
-            val (x,y) = dragAmount
-            when {
-                x > 0 -> { isSwipeRight = true
-                    Log.d("CarouselPager", "Swiped Right")}
-                x < 0 -> { isSwipeRight = false
-                    Log.d("CarouselPager", "Swiped Left")}
-            }
-        }
+    .fillMaxSize()
+    .pointerInput(Unit) {
+    detectDragGestures { change, dragAmount ->
+    change.consume()
+    val (x,y) = dragAmount
+    when {
+    x > 0 -> { isSwipeRight = true
+    Log.d("CarouselPager", "Swiped Right")}
+    x < 0 -> { isSwipeRight = false
+    Log.d("CarouselPager", "Swiped Left")}
+    }
+    }
     }) {}
-    */
+     */
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .fillMaxWidth()
-        .background(color = Color.Black))
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxWidth()
+            .background(color = Color.Black)
+    )
     {
         val items = createItems()
         val pagerState = rememberPagerState()
+        val coroutineScope = rememberCoroutineScope()
 
-        Box(modifier = Modifier
-            .background(Color.Black)
-            .padding(16.dp))
+        var speed by remember {
+            mutableStateOf(1F)
+        }
+
+        fun nextPage(): Int {
+            return if (pagerState.currentPage < 4) pagerState.currentPage + 1 else 4
+        }
+        fun prevPage(): Int {
+            return if (pagerState.currentPage > 0) pagerState.currentPage - 1 else 0
+        }
+
+        fun speedChange(newSpeed: Float): Unit {
+            coroutineScope.launch {
+                speed = newSpeed
+                if (speed < 0) {
+                    pagerState.scrollToPage(prevPage())
+                } else if (speed > 0) {
+                    pagerState.scrollToPage(nextPage())
+                }
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .background(Color.Black)
+                .padding(16.dp)
+        )
         {
             // Lottie Animation
-            LottieAnimationContent(currentPage = pagerState.currentPage, targetPage = pagerState.targetPage)
+            LottieAnimationContent(currentPage = pagerState.currentPage, speed = speed)
 
             // Full Screen Horizontal Pager
             HorizontalPagerScreen(items, pagerState)
@@ -78,33 +103,56 @@ fun CarouselPagerScreen() {
             // UI Content
             Column {
                 // Top Row
-                Box(modifier = Modifier
-                    .weight(0.1f)
-                    .background(Color.Black)) {
+                Box(
+                    modifier = Modifier
+                        .weight(0.1f)
+                        .background(Color.Black)
+                ) {
                     TopRowContent()
                 }
                 // Adds additional space
                 Box(modifier = Modifier.weight(0.5f)) {
                 }
                 // Pager Indicator
-                Box (modifier = Modifier
-                    .weight(0.1f)
-                    .align(Alignment.CenterHorizontally)) {
-                     PagerIndicator(pagerState = pagerState)
+                Box(
+                    modifier = Modifier
+                        .weight(0.1f)
+                        .align(Alignment.CenterHorizontally)
+                ) {
+                    Column {
+                        PagerIndicator(pagerState = pagerState)
+                        Text(
+                            modifier = Modifier
+                                .background(Color.LightGray),
+                            color = Color.Black,
+                            text = "PAGE: ${pagerState.currentPage}"
+                        )
+                    }
                 }
                 // Text Content
                 Crossfade(
                     targetState = pagerState.currentPage,
                     animationSpec = tween(2000),
-                    modifier = Modifier.weight(0.2f)) { it ->
+                    modifier = Modifier.weight(0.2f)
+                ) { it ->
                     Box {
                         TextContent(items = items, currentPage = it)
                     }
                 }
                 // Sign Up Button
-                Box(modifier = Modifier
-                    .weight(0.1f)) {
-                    SignUpButton()
+                Box(
+                    modifier = Modifier
+                        .weight(0.1f)
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    ) {
+                        SignUpButton("back", speedChange = ::speedChange)
+                        SignUpButton("next", speedChange = ::speedChange)
+                    }
                 }
             }
         }
@@ -115,7 +163,8 @@ fun CarouselPagerScreen() {
 @Composable
 fun HorizontalPagerScreen(
     items: List<HorizontalPagerContent>,
-    pagerState: PagerState) {
+    pagerState: PagerState
+) {
     HorizontalPager(
         count = items.size,
         state = pagerState,
@@ -137,10 +186,12 @@ fun HorizontalPagerScreen(
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun TopRowContent() {
-    Row(horizontalArrangement = Arrangement.SpaceBetween,
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
-            .fillMaxSize())
+            .fillMaxSize()
+    )
     {
         val context = LocalContext.current
         // Removes extra 48dp touch target padding around the Icon Button
@@ -148,11 +199,12 @@ fun TopRowContent() {
             IconButton(
                 onClick = {
                     Toast.makeText(context, "Pressed Up Button.", Toast.LENGTH_SHORT).show()
-            }) {
+                }) {
                 Icon(
                     Icons.Filled.KeyboardArrowLeft,
                     "Back Arrow",
-                    tint = Color.White)
+                    tint = Color.White
+                )
             }
         }
         Text(
@@ -161,17 +213,28 @@ fun TopRowContent() {
             color = Color.White,
             modifier = Modifier.clickable {
                 Toast.makeText(context, "Pressed Sign-In Button.", Toast.LENGTH_SHORT).show()
-        })
+            })
     }
 }
 
+val firstClipSpec = LottieClipSpec.Progress(min = 0f, max = 0.19f)
+val secondClipSpec = LottieClipSpec.Progress(min = 0.19f, max = 0.38f)
+val thirdClipSpec = LottieClipSpec.Progress(min = 0.38f, max = 0.57f)
+val fourthClipSpec = LottieClipSpec.Progress(min = 0.57f, max = 0.76f)
+val fifthClipSpec = LottieClipSpec.Progress(min = 0.76f, max = 1.0f)
+
 @Composable
-fun LottieAnimationContent(currentPage: Int, targetPage: Int) {
-    val firstClipSpec = LottieClipSpec.Progress(min = 0f, max = 0.19f)
-    val secondClipSpec = LottieClipSpec.Progress(min = 0.19f, max = 0.38f)
-    val thirdClipSpec = LottieClipSpec.Progress(min = 0.38f, max = 0.57f)
-    val fourthClipSpec = LottieClipSpec.Progress(min = 0.57f, max = 0.76f)
-    val fifthClipSpec = LottieClipSpec.Progress(min = 0.76f, max = 1.0f)
+fun LottieAnimationContent(currentPage: Int, speed: Float) {
+
+    Log.d("ZZZ", "ENTER LottieAnimationContent()")
+
+    var prevPage by remember {
+        mutableStateOf(-1)
+    }
+
+    var prevSpeed by remember {
+        mutableStateOf(1F)
+    }
 
     val clipSpec = remember {
         mutableStateOf<LottieClipSpec?>(firstClipSpec)
@@ -181,26 +244,23 @@ fun LottieAnimationContent(currentPage: Int, targetPage: Int) {
         mutableStateOf<LottieCompositionSpec>(LottieCompositionSpec.RawRes(R.raw.onboarding_carousel_test))
     }
 
+    lottieComposition.value = LottieCompositionSpec.RawRes(R.raw.onboarding_carousel_test)
+
     when (currentPage) {
         0 -> {
             clipSpec.value = firstClipSpec
-            lottieComposition.value = LottieCompositionSpec.RawRes(R.raw.onboarding_carousel_test)
         }
         1 -> {
             clipSpec.value = secondClipSpec
-            lottieComposition.value = LottieCompositionSpec.RawRes(R.raw.onboarding_carousel_test)
         }
         2 -> {
             clipSpec.value = thirdClipSpec
-            lottieComposition.value = LottieCompositionSpec.RawRes(R.raw.onboarding_carousel_test)
         }
         3 -> {
             clipSpec.value = fourthClipSpec
-            lottieComposition.value = LottieCompositionSpec.RawRes(R.raw.onboarding_carousel_test)
         }
         4 -> {
             clipSpec.value = fifthClipSpec
-            lottieComposition.value = LottieCompositionSpec.RawRes(R.raw.onboarding_carousel_test)
         }
     }
     // TODO: Figure out the direction of swipe to play the partial animations in normal or reverse
@@ -216,21 +276,52 @@ fun LottieAnimationContent(currentPage: Int, targetPage: Int) {
 //    Log.d("CarouselPager", "targetPage: $targetPage, currentPage: $currentPage")
 //    Log.d("CarouselPager", "direction: $direction")
 
+
+//    var speed = prevSpeed
+//
+//    if (currentPage != prevPage) {
+//        Log.d("ZZZ", "There is a CHANGE")
+//
+//        if (currentPage > prevPage) {
+//            Log.d("ZZZ", "Setting speed to 1F")
+//            speed = 1F
+//        } else {
+//            // speed = if (currentPage == 0) 0F else -1F
+//            Log.d("ZZZ", "Setting speed to -1F")
+//            speed = -1F
+//        }
+//        prevPage = currentPage
+//        prevSpeed = speed
+//
+//
+//    } else {
+//        Log.d("ZZZ", "There is NO CHANGE ---------------------")
+//
+//    }
+
+    Log.d("ZZZ", "currentPage: $currentPage  speed: $speed")
+
+
     val progress by animateLottieCompositionAsState(
         composition = composition,
         clipSpec = clipSpec.value,
+        iterations = 1,
+
         /**
          * speed is the parameter that controls the animation will be played in normal or reverse
          * 1.0f for normal and -1.0f is for reverse
          */
-        speed = 1.0f
+//        speed = 1.0f
+//        speed = (targetPage - currentPage).toFloat()
+//        speed = newSpeed
+        speed = speed
     )
 
     LottieAnimation(
         composition = composition,
         progress = { progress },
-        modifier = Modifier
-        .background(Color.Black)
+//        modifier = Modifier
+//            .background(Color.Black)
     )
 }
 
@@ -270,23 +361,22 @@ fun TextContent(items: List<HorizontalPagerContent>, currentPage: Int) {
 }
 
 @Composable
-fun SignUpButton() {
-    Row(horizontalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()) {
-        Button(
-            onClick = {},
-            shape = RoundedCornerShape(10.dp),
-            colors = ButtonDefaults.buttonColors(Color.Blue),
-            modifier = Modifier.width(150.dp)
-        ) {
-            Text(text = "Sign Up", fontSize = 16.sp, color = Color.White, textAlign = TextAlign.Center)
-        }
+fun SignUpButton(label: String, speedChange: (sp: Float) -> Unit) {
+    Button(
+        onClick = {
+            if (label == "back") speedChange(-1F) else speedChange(1F)
+        },
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.buttonColors(Color.Blue),
+        modifier = Modifier.width(150.dp)
+    ) {
+        Text(text = label, fontSize = 16.sp, color = Color.White, textAlign = TextAlign.Center)
     }
 }
 
 private fun createItems() = listOf(
     HorizontalPagerContent(
-        title = "Share your ideas whenever and where ever." ,
+        title = "Share your ideas whenever and where ever.",
         description = "Ideas worth spreading. No boundaries are good enough. Let's go infinity and beyond!",
     ),
     HorizontalPagerContent(
